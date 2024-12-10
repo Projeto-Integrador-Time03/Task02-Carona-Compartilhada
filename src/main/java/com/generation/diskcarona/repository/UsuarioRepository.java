353950da3a1e5c5fc9556606bfc0117cb40845b5
fany-santos8 <estefany_fanysantos@hotmail.com>
2024-12-10 10:34:03 -0300

    criação do model e repository

--- src/main/java/com/generation/diskcarona/repository/UsuarioRepository.java
+++ src/main/java/com/generation/diskcarona/repository/UsuarioRepository.java
@@ -1,5 +1,12 @@
 package com.generation.diskcarona.repository;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.generation.diskcarona.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+ 
+	
 }
